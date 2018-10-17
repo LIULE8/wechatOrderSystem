d8edf2c3d6168e5286f9c4e7dc26d50595103891
LIULE9 <LEO.ZHEN.CONG.LIU@OOCL.COM>
2018-10-17 16:17:41 +0800

    1.delete mapper

--- src/main/java/com/imooc/SellApplication.java
+++ src/main/java/com/imooc/SellApplication.java
@@ -1,6 +1,5 @@
 package com.imooc;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -9,7 +8,6 @@ import org.springframework.cache.annotation.EnableCaching;
  * @author LIULE9
  */
 @SpringBootApplication
-@MapperScan(basePackages = "com.imooc.dataobject.mapper")
 @EnableCaching
 public class SellApplication {
 
